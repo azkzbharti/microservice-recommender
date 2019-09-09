b3c966b87da52164e35966b1bf35b2dceceffe2d
shreya <skhare34@in.ibm.com>
2019-09-09 13:53:32 +0530

    example code for metric computation

---
+++ src/main/java/com/ibm/research/msr/expandcluster/MetricComputation.java
@@ -0,0 +1,48 @@
+package com.ibm.research.msr.expandcluster;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ibm.research.msr.clustering.ClusterDetails;
+import com.ibm.research.msr.extraction.Document;
+
+public class MetricComputation {
+	List<ClusterDetails> listofclusters=new ArrayList<>() ;
+	List<ClusterDetails> newlistofclusters=new ArrayList<>() ;
+	
+	static InterClassUsageFinder i = new InterClassUsageFinder();
+	
+	
+	public void getMetrics() {
+		for(ClusterDetails cls:listofclusters) {
+			//computeMetrics();
+		}
+		for(ClusterDetails cls:listofclusters) {
+			//reassignMetrics();
+		
+		
+		// example of getting docUsageMap from Interclass 
+		Map<String,Integer> docUsageMap = new HashMap<>();
+		
+		for (Document doc :cls.getListOfDocuments()) {
+	
+		List<ClassPair> cp=i.getAssociatedClassPairForClass(doc.getUniqueName());
+		
+		for (ClassPair pair : cp) {
+			if(!pair.thisClass.equals(doc.getUniqueName())) {
+				docUsageMap.put(pair.thisClass, i.interClassUsageMatrix.get(pair));
+			}
+			if(!pair.usedClass.equals(doc.getUniqueName())) {
+				docUsageMap.put(pair.usedClass, i.interClassUsageMatrix.get(pair));
+			}
+
+		}
+		
+		}
+		}
+		
+	}
+
+}
