eed04c49db58ab9aa9c06012e732cfa02981901e
Giriprasad Sridhara <girisrid@in.ibm.com>
2019-09-19 11:39:25 +0530

    google custom search when the jar does not have grup id/artifact id, also added title as sometimes categories,tags are empty as in case of hibernate-jpa-2.1-api

--- src/main/java/com/ibm/research/msr/jarlist/MavenCategoryEtAlExtractor.java
+++ src/main/java/com/ibm/research/msr/jarlist/MavenCategoryEtAlExtractor.java
@@ -1,9 +1,14 @@
 package com.ibm.research.msr.jarlist;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
@@ -18,6 +23,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 
 /**
  * extracts category, tags et al from a jar by going to maven repo website
@@ -35,6 +46,10 @@ public class MavenCategoryEtAlExtractor {
 	 */
 	class MavenCategoryEtAl
 	{
+		StringBuffer title=new StringBuffer();
+		
+//		StringBuffer subTitle=new StringBuffer();
+		
 		StringBuffer description=new StringBuffer();
 		
 		StringBuffer license=new StringBuffer();
@@ -47,19 +62,33 @@ public class MavenCategoryEtAlExtractor {
 		
 		String name = new String();
 
-		public MavenCategoryEtAl(String description, String license, String categories, String tags, String usedBy) {
-			super();
-			this.description.append(description + " ");
-			this.license.append(license + " ");
-			this.categories.append(categories + " ");
-			this.tags.append(tags + " ");
-			this.usedBy.append(usedBy + " ");
-		}
+//		public MavenCategoryEtAl(String description, String license, String categories, String tags, String usedBy) {
+//			super();
+//			this.description.append(description + " ");
+//			this.license.append(license + " ");
+//			this.categories.append(categories + " ");
+//			this.tags.append(tags + " ");
+//			this.usedBy.append(usedBy + " ");
+//		}
 		
 		public MavenCategoryEtAl(String name) {
 			// TODO Auto-generated constructor stub
 			this.name = name;
 		}
+
+		/**
+		 * @param title2 the title to set
+		 */
+		public void setTitle(String title2) {
+			this.title.append(title2);
+		}
+
+//		/**
+//		 * @param subTitle the subTitle to set
+//		 */
+//		public void setSubTitle(String subTitle) {
+//			this.subTitle.append(subTitle);
+//		}
 
 		public void setDescription(String d)
 		{
@@ -68,6 +97,8 @@ public class MavenCategoryEtAlExtractor {
 		
 		public void setNameValuePairs(String n,String v)
 		{
+			System.out.println("setNameValuePairs n,v="+n+" : "+v);
+			
 			if (n.compareTo("License")==0)
 			{
 				license.append(v+ " ");
@@ -87,11 +118,17 @@ public class MavenCategoryEtAlExtractor {
 		}
 	}
 	
+	// TODO: put these in properties file and read
+	final String DEFAULT_API_KEY="AIzaSyBtBf1TYvn041OOrxcLRJFUQsafijHcnAw";
+
+	final String MVNREPOSITORY_CUSTOM_SEARCH_ENGINE_ID="017382220073484135075:jyl9yuofxnf";
+
+	
 	public void find(String jarRootPath, String outputJSONFile) {
 
 		File fRoot = new File(jarRootPath);
 		String[] extensions = new String[] { "jar" };
-		System.out.println("Getting all .jar  in " + fRoot.getPath() + " including those in subdirectories");
+		//System.out.println("Getting all .jar  in " + fRoot.getPath() + " including those in subdirectories");
 		List<File> files = (List<File>) FileUtils.listFiles(fRoot, extensions, true);
 		Set<String> processedJars = new HashSet<String>();
 
@@ -112,7 +149,13 @@ public class MavenCategoryEtAlExtractor {
 			MavenCategoryEtAl m = processOneJar(jarName);
 			if (m != null) {
 
+				// TODO: not sure if this kind of JSON object creation
+				// etc is required, we can directly use Gson in fact
+				// that is the whole purpose of creating the data holder
+				// class above
 				JSONObject mObject = new JSONObject();
+				mObject.put("title", m.title.toString());
+//				mObject.put("subTitle", m.subTitle.toString());
 				mObject.put("description", m.description.toString());
 				mObject.put("license", m.license.toString());
 				mObject.put("categories", m.categories.toString());
@@ -193,88 +236,33 @@ public class MavenCategoryEtAlExtractor {
                 {
                 	continue;
                 }
+                
+                
                 if (groupId==null || artifactId==null)
                 {
                 	System.err.println("group/artifact id null " + groupId + " , "+artifactId + " for JAR " + jarName);
-                	return null;
+                	String sUrl=findMavenRepositoryHomePageByGoogleSearch(jarWoPath);
+	                extractFromHTMLPage(sUrl, m);
+	    			return m;
+                	//return null;
                 }
-                
-                // for url like:
-                //https://mvnrepository.com/artifact/com.google.guava/guava
-                String sUrl="https://mvnrepository.com/artifact/"+groupId+"/"+artifactId;
-    			Document d = Jsoup.connect(sUrl).get();
-    			System.out.println(d.html());
-    			
-    			String descText=null;
-    			Elements desc = d.select("div.im-description");
-    			if (desc !=null)
-    			{
-    				Element descElem = desc.get(0);
-    				if (descElem!=null)
-    				{
-    					System.out.println("description="+descElem.text());
-    					descText=descElem.text();
-    					m.setDescription(descText);
-    				}
-    			}
-    			
-    			Elements tables=d.select(".grid");
-    			if (tables!=null)
-    			{
-    				Element gridTable = tables.get(0);
-    				if (gridTable != null)
-    				{
-    					Elements rows = gridTable.select("tr");
-
-    				    for (int i = 0; i < rows.size(); i++) { 
-    				        Element row = rows.get(i);
-    				        Elements th = row.select("th");
-    				        Elements td = row.select("td");
-
-    				        String rowName=th.get(0).text();
-    				        System.out.println("th="+rowName);
-    				        
-    				        for (int j=0;j<td.size();j++)
-    				        {
-    				        	Element el = td.get(j);
-    				        	System.out.println("td " + j + " " + el.text());
-    				        	// <th>Tags</th>
-    				        	// <td><a href="/tags/command-line" class="b tag">command-line</a><a href="/tags/cli" class="b tag">cli</a><a href="/tags/parser" class="b tag">parser</a></td>
-    				        	String rowVal=el.text();
-    				        	// TODO: check if this needs to be done even for "Categories"
-    				        	if (rowName.compareTo("Tags")==0)
-    				        	{
-    				        		StringBuffer sbTags=new StringBuffer();
-    				        		Elements tags = el.select("a");
-    				        		for (Element t:tags)
-    				        		{
-    				        			System.out.println("\t tc="+t.text() + " :href="+t.attr("href"));
-    				        			sbTags.append(t.text() + " ");
-    				        		}
-    				        		m.setNameValuePairs(rowName, sbTags.toString());
-    				        	}
-    				        	else
-    				        	{
-    				        		m.setNameValuePairs(rowName, rowVal);
-    				        	}
-    				        }
-    				        
-    				        //System.out.println("td="+td.get(0).text());
-    				        //if (cols.get(3).text().equals("Titan")) {
-    				        	
-    				        //}
-    				        
-    				    }	
-    				}
-    			}
-    			
-    			return m;
+                else
+                {
+	                // for url like:
+	                //https://mvnrepository.com/artifact/com.google.guava/guava
+	                String sUrl="https://mvnrepository.com/artifact/"+groupId+"/"+artifactId;
+	                extractFromHTMLPage(sUrl, m);
+	    			return m;
+                }
             }
 
             if (groupId==null || artifactId==null)
             {
             	System.err.println("group/artifact id null " + groupId + " , "+artifactId);
-            	return null;
+            	String sUrl=findMavenRepositoryHomePageByGoogleSearch(jarWoPath);
+                extractFromHTMLPage(sUrl, m);
+    			return m;
+            	//return null;
             }
 
 		} catch (IOException e) {
@@ -296,6 +284,166 @@ public class MavenCategoryEtAlExtractor {
 		return m;
 
 	}
+
+	public void extractFromHTMLPage(String sUrl, MavenCategoryEtAl m)
+	{
+		Document d=null;
+		try {
+			d = Jsoup.connect(sUrl).get();
+			//System.out.println(d.html());
+			
+			String descText=extractDivText(d, "div.im-description");
+			m.setDescription(descText);
+			
+			String title=extractDivText(d, "h2.im-title");
+			m.setTitle(title);
+
+//			String subTitle=extractDivText(d, "im-subtitle");
+//			m.setSubTitle(subTitle);
+			
+			
+			Elements tables=d.select(".grid");
+			if (tables!=null)
+			{
+				Element gridTable = tables.get(0);
+				if (gridTable != null)
+				{
+					Elements rows = gridTable.select("tr");
+	
+				    for (int i = 0; i < rows.size(); i++) { 
+				        Element row = rows.get(i);
+				        Elements th = row.select("th");
+				        Elements td = row.select("td");
+	
+				        String rowName=th.get(0).text();
+				   //     System.out.println("th="+rowName);
+				        
+				        for (int j=0;j<td.size();j++)
+				        {
+				        	Element el = td.get(j);
+				     //   	System.out.println("td " + j + " " + el.text());
+				        	// <th>Tags</th>
+				        	// <td><a href="/tags/command-line" class="b tag">command-line</a><a href="/tags/cli" class="b tag">cli</a><a href="/tags/parser" class="b tag">parser</a></td>
+				        	String rowVal=el.text();
+				        	// TODO: check if this needs to be done even for "Categories"
+				        	if (rowName.compareTo("Tags")==0)
+				        	{
+				        		StringBuffer sbTags=new StringBuffer();
+				        		Elements tags = el.select("a");
+				        		for (Element t:tags)
+				        		{
+				       // 			System.out.println("\t tc="+t.text() + " :href="+t.attr("href"));
+				        			sbTags.append(t.text() + " ");
+				        		}
+				        		m.setNameValuePairs(rowName, sbTags.toString());
+				        	}
+				        	else
+				        	{
+				        		m.setNameValuePairs(rowName, rowVal);
+				        	}
+				        }
+				        
+				        //System.out.println("td="+td.get(0).text());
+				        //if (cols.get(3).text().equals("Titan")) {
+				        	
+				        //}
+				        
+				    }	
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public String findMavenRepositoryHomePageByGoogleSearch(String query)
+	{
+		
+		String surl="https://www.googleapis.com/customsearch/v1?key="+DEFAULT_API_KEY +
+				"&cx="+MVNREPOSITORY_CUSTOM_SEARCH_ENGINE_ID+"&q="+query+"&alt=json";
+		System.out.println(surl);
+		try {
+			URL url=new URL(surl);
+			HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+			uc.setRequestMethod("GET");
+			uc.setRequestProperty("Accept", "application/json");
+			
+			int rc=uc.getResponseCode();
+			if (rc != 200)
+			{
+				System.err.println("Unable to search. Error code ="+rc);
+				return "";
+			}
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(uc.getInputStream()));
+
+			String line=null;
+			StringBuffer resultJSON=new StringBuffer();
+			while ((line=br.readLine())!=null)
+			{
+				resultJSON.append(line+"\n");
+			}
+			System.out.println("Google Custom Searcher response="+resultJSON);
+			uc.disconnect();
+			
+			Gson g=new Gson();
+			JsonObject jo=g.fromJson(resultJSON.toString(), JsonObject.class);
+			JsonArray ja= jo.getAsJsonArray("items");
+
+			int size=ja.size();
+			for (int i=0;i<size;i++)
+			{
+				JsonElement je = ja.get(i);
+				JsonObject jeo = je.getAsJsonObject();
+	
+				JsonElement l = jeo.get("link");
+				//System.out.println(i+"\t link="+l.getAsString());
+				String link=l.getAsString();
+				System.out.println(i+"\t"+link);
+			}
+
+			
+			// just get the first result.
+			JsonElement je = ja.get(0);
+			JsonObject jeo = je.getAsJsonObject();
+			JsonElement l = jeo.get("link");
+			//System.out.println(i+"\t link="+l.getAsString());
+			String link=l.getAsString();
+			System.out.println("link to use="+link);
+			return link;
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
+	public String extractDivText(Document d, String divClass)
+	{
+		String text="";
+		
+		try {
+			//Elements desc = d.select("div.im-description");
+			Elements desc = d.select(divClass);
+			if (desc != null) {
+				Element descElem = desc.get(0);
+				if (descElem != null) {
+					//	System.out.println("description="+descElem.text());
+					text = descElem.text();
+					//m.setDescription(descText);
+					return text;
+				}
+			} 
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return text;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -316,11 +464,15 @@ public class MavenCategoryEtAlExtractor {
 		}
 		else
 		{
-			String jarName="C:\\Users\\GiriprasadSridhara\\Documents\\demo-july\\july-30\\guava-27.1-jre.jar";
+			//String jarName="C:\\Users\\GiriprasadSridhara\\Documents\\demo-july\\july-30\\guava-27.1-jre.jar";
 			//String jarName="C:\\Users\\GiriprasadSridhara\\.m2\\repository\\commons-cli\\commons-cli\\1.4\\commons-cli-1.4.jar";
+			String jarName="C:\\Users\\GiriprasadSridhara\\sample.daytrader7\\daytrader-ee7-ejb\\msr-output3\\temp\\jars\\hibernate-jpa-2.1-api-1.0.2.jar";
 			//String opJsonFileName="C:\\temp\\maven-category-etal.json";
 			String opRoot="C:\\temp";
-			m.processOneJar(jarName);
+			MavenCategoryEtAl mavenCatEtAl = m.processOneJar(jarName);
+			Gson g=new Gson();
+			String sjson=g.toJson(mavenCatEtAl);
+			System.out.println(sjson);
 		}
 	}
 
